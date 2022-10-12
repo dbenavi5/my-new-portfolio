179c3aaf4729184ff0c1ad61bd82d775a0bbce80
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2022-10-12 12:46:23 -0700

    finish header/navbar implementation&design

---
+++ components/Header.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable no-empty-pattern */
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from 'framer-motion';
+
+type Props = {};
+
+export default function Header({ }: Props) {
+    return (
+        <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+            <motion.div
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.5,
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale:1,
+                }}
+                transition={{
+                    duration:1.5,
+                }}
+                className="flex flex-row items-center">
+                {/* Social Icons */}
+                <SocialIcon
+                    url="https://linkedin.com/in/diana-c-benavides"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <SocialIcon
+                    url="https://github.com/dbenavi5"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <SocialIcon
+                    url="https://www.facebook.com/dbenavi5/"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <SocialIcon
+                    url="https://twitter.com/dianab_415"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <SocialIcon
+                    url="https://www.pinterest.com/dbenavidb/"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+            </motion.div>
+            {/* Navbar */}
+            <motion.div
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale: 0.5,
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale:1,
+                }}
+                transition={{
+                    duration:1.5,
+                }}
+                className='flex flex-row items-center text-gray-300 cursor-pointer'>
+                <SocialIcon
+                    className="cursor-pointer"
+                    network="email"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in touch</p>
+            </motion.div>
+        </header>
+    );
+}
