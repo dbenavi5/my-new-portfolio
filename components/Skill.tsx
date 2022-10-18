762c35bbac2d2d15234b05da650c3fda789c81a7
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2022-10-18 02:43:36 -0700

    add  % to skill component

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -22,7 +22,7 @@ const Skill = ({skills,directionLeft}: Props) => {
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-20 h-20 md:w-24 md:h-24 xl:w-28 xl:h-28 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
           <p className="text-3xl font-bold text-black opacity-100">
-            {skills.progress}
+            {skills.progress}%
           </p>
         </div>
       </div>
